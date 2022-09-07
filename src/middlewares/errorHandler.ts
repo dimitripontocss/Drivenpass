6f74027315c59991d38173fab7612b4375376384
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-07 20:25:37 -0300

    Project ready without bonusses

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -3,14 +3,18 @@ import { Request, Response, NextFunction } from "express";
 import { IError } from "../interfaces/interfaces.js";
 
 export default function errorHandlingMiddleware(error: IError | Error, req: Request, res: Response, next: NextFunction) {
+	console.log(error.name);
 	if(error.name === "Already used"){
 		return res.status(409).send(error.message);
 	}
 	if(error.name === "Login_Error"){
 		return res.status(404).send(error.message);
 	}
+	if(error.name === "JsonWebTokenError"){
+		return res.status(401).send(error.message);
+	}
 	if(error.name === "Not_Authorized"){
-		return res.status(404).send(error.message);
+		return res.status(401).send(error.message);
 	}
 	if(error.name === "Unexistent"){
 		return res.status(404).send(error.message);
